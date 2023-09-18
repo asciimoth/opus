f640e2b4dc13a2491d0adb45fc5f1fe9e37f70bb
AsciiMoth <ascii@moth.contact>
2023-09-18 14:37:24 +0400

    Enable alejandra linter

--- flake.nix
+++ flake.nix
@@ -12,39 +12,39 @@
     pre-commit-hooks,
     ...
   }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        config = {
-          permittedInsecurePackages = ["wasm3-0.5.0"];
-        };
-      };
-      checks = {
-        pre-commit-check = pre-commit-hooks.lib.${system}.run {
-          src = ./.;
-          hooks = {
-            nixpkgs-fmt.enable = true;
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            permittedInsecurePackages = ["wasm3-0.5.0"];
           };
         };
-      };
-      devShell = nixpkgs.legacyPackages.${system}.mkShell {
-        inherit (self.checks.${system}.pre-commit-check) shellHook;
-      };
-    in {
-      devShell = pkgs.mkShell {
-        buildInputs = [
-          # Python toolchain
-          pkgs.poetry
-          pkgs.mypy
-          # Command runner
-          pkgs.just
-          # CLI hex files viewer
-          pkgs.hexd
-          # Wasm toolchain
-          pkgs.wabt
-          pkgs.wasmer
-          #pkgs.wasm3
-        ];
-      };
-    });
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              alejandra.enable = true;
+            };
+          };
+        };
+      in {
+        devShell = pkgs.mkShell {
+          inherit (checks.pre-commit-check) shellHook;
+          buildInputs = [
+            # Python toolchain
+            pkgs.poetry
+            pkgs.mypy
+            # Command runner
+            pkgs.just
+            # CLI hex files viewer
+            pkgs.hexd
+            # Wasm toolchain
+            pkgs.wabt
+            pkgs.wasmer
+            pkgs.wasm3
+          ];
+        };
+      }
+    );
 }
